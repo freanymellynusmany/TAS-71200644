a4a27644da57912a0fbf6b842d084e3ae79a2d93
freanymellynusmany <78341427+freanymellynusmany@users.noreply.github.com>
2022-06-20 17:36:56 +0700

    Add files via upload

---
+++ TokoMonster/src/main/java/com/uas/prakrplbo/Manticore.java
@@ -0,0 +1,20 @@
+package com.uas.prakrplbo;
+
+public class Manticore extends Monster {
+
+
+    public Manticore(String nama){
+        super(nama);
+    }
+
+    public String bergerak(){
+        return ("Terbang");
+    }
+    public String bersuara(){
+        return ("Roarroar");
+    }
+
+
+
+
+}
